20099fb032fedc9b13d91cada6bd164cf1b32f67
mariankh <mariankh@bitbucket.org>
2017-07-20 16:41:44 +0300

    updates

--- linphonemodificationfiles/startcall.sh
+++ linphonemodificationfiles/startcall.sh
@@ -2,7 +2,9 @@
 ##   --verbose, -v  Verbose mode
 ##   --dial=number  Output filename
 
+
 source easyoptions || exit
+./ffasn1dump msd.asn -I gser ECallMessage msd.gser -O aper msd.per
 ./linphonec -s "${dial}"
 
 
